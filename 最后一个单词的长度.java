58e97c45b5097ece7e217ac633601593b433c8c2
kzhang19 <65601837+zhangkai0425@users.noreply.github.com>
2022-04-09 01:27:34 +0800

    Create 最后一个单词的长度.java

---
+++ 最后一个单词的长度.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int index = s.length() - 1;
+        while (s.charAt(index) == ' ') {
+            index--;
+        }
+        int wordLength = 0;
+        while (index >= 0 && s.charAt(index) != ' ') {
+            wordLength++;
+            index--;
+        }
+        return wordLength;
+    }
+}
