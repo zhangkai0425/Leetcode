c169eae73fabbd96c6050e2722b29f4a38a481bd
zhangkai0425 <1349605984@qq.com>
2022-08-29 11:08:04 +0800

    细心认真就行!

--- THU-OJ/OJ1.cpp
+++ THU-OJ/OJ1.cpp
@@ -16,15 +16,16 @@ int main()
     unordered_map<int, int> MA, MB;
     vector<int> weights1, weights2;
     vector<pair<int, int>> ans;
-    int n1, n2, target, tmp, parent, child;
+    int n1, n2, target, tmp, t1, t2;
     // input tree 1
     cin >> n1;
     for (int i = 0; i < n1; i++)
         cin >> tmp, weights1.push_back(tmp);
     for (int i = 0; i < n1 - 1; i++)
     {
-        cin >> parent >> child;
-        MA[parent]++;
+        cin >>t1 >> t2;
+        MA[t1]++;
+        MA[t2]++;
     }
     // input tree 2
     cin >> n2;
@@ -32,19 +33,37 @@ int main()
         cin >> tmp, weights2.push_back(tmp);
     for (int i = 0; i < n2 - 1; i++)
     {
-        cin >> parent >> child;
-        MB[parent]++;
+        cin >> t1 >> t2;
+        MB[t1]++;
+        MB[t2]++;
     }
     cin >> target;
     // search for the target in the x + y
+    MA[1] += 1,MB[1] += 1; // root node especial
     for (int x = 1; x <= n1; x++)
-        for (int y = 1; y <= n2; y++)
-            if (MA[x] != 2 || MB[x] != 2)
-                continue;
-            else if (weights1[x - 1] + weights2[y - 1] == target)
+        for (int y = 1; y <= n2; y++){
+            if(MA[x] != 3 || MB[y] != 3) continue;
+            else if (weights1[x - 1] + weights2[y - 1] == target){
                 ans.push_back(make_pair(x, y));
+            }  
+        }
     for (auto p : ans)
         cout << p.first << " " << p.second << endl;
     if (ans.size() == 0)
         cout << -1 << endl;
 }
+/*
+7
+1 1 1 1 1 1 1
+1 2
+1 3
+2 4
+2 5
+3 6
+3 7
+3
+1 1 1
+1 2
+1 3
+2
+*/
